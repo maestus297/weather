00dfd90f589e1b4c9808869ffb477ce1968dbbc9
eirin <a.vishnu2097@gmail.com>
2021-03-01 03:07:17 +0530

    added some extra variables

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ async function weather (key) {
     const response = await fetch(key, {mode: 'cors'});
     const weatherData = await response.json();
     console.log(weatherData);
+    
+  //  let temp = weatherData.main.temp;
+    //console.log(temperature); 
+    // let temperature = document.getElementById("temp");
+   // let temp = document.createTextNode(weatherData.main.temp);
+  //  temperature.appendChild(temp);
+
+    document.querySelector('.temp').textContent = weatherData.main.temp + "°";
+    document.querySelector('.location').textContent = weatherData.name;
+    document.querySelector('.minTemp').textContent = weatherData.main.temp_max;
+    document.querySelector('.maxTemp').textContent = weatherData.main.temp_max;
+    document.querySelector('.feelsLike').textContent = weatherData.main.feels_like;
+
+  
+    //weatherdata.main.temp.feels_like, weatherdata.main.temp, weatherdata.main.temp_max, weatherdata.main.temp_min ];
 }
 
 
