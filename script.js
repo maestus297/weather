adfcb9cd99c02343f741345c6d2bfef13c8efc26
eirin <a.vishnu2097@gmail.com>
2021-03-03 18:35:14 +0530

    Made extra changes. Finished setting the layout.

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
-let city = 'London'; 
+//let location = 'london'; 
 
-async function weather (city) {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=5130b57b1681d2293bbd618c702c6178`, {mode: 'cors'});
+async function weather (search) {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${search}&APPID=5130b57b1681d2293bbd618c702c6178`, {mode: 'cors'});
     const weatherData = await response.json();
 
     console.log(weatherData);
     
-  //  let temp = weatherData.main.temp;
-    //console.log(temperature); 
-    // let temperature = document.getElementById("temp");
-   // let temp = document.createTextNode(weatherData.main.temp);
-  //  temperature.appendChild(temp);
-  let temperature, location, feelsLike;
+  let city = weatherData.name;
+  let country = weatherData.sys.country;
+  let condition =  weatherData.weather[0].main
+  let temperature = parseInt(weatherData.main.temp - 273.15);
+  let feelsLike = parseInt(weatherData.main.feels_like - 273.15);
+  let wind = parseInt(weatherData.wind.speed);
+  let humidity = weatherData.main.humidity;
+  let location = `${city}, ${country}`;
 
- temperature = parseInt(weatherData.main.temp - 273.15);
- city = weatherData.name;
- feelsLike =  parseInt(weatherData.main.feels_like - 273.15);
- let country = weatherData.sys.country;
- // let looksLike = weatherData.weather.
-
- document.querySelector('.temp').textContent = temperature + "°C";
- document.querySelector('.city').textContent = city;
- document.querySelector('.country').textContent = country;
- document.querySelector('.feelsLike').textContent = feelsLike + "°C";
-
- console.log(weatherData.weather[0].main);
+ 
+  document.querySelector('.location').textContent = location;
+  document.querySelector('.condition').textContent = condition;
+  document.querySelector('.temp').textContent = temperature;
+  document.querySelector('.feels-like').textContent = `Feels Like: ${feelsLike}`;
+  document.querySelector('.wind').textContent = `Wind: ${wind}m/s`
+  document.querySelector('.humidity').textContent = `Humidity: ${humidity}`;
 };
 
-
-
-
-weather(city);
+weather('london');
